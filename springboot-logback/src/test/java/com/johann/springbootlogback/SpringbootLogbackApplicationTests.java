7800ea49a7aa6b38edf02ba169c63f4a482a9434
Johann <yhzhaohy@hotmail.com>
2020-09-15 22:47:52 +0800

    logback

---
+++ springboot-logback/src/test/java/com/johann/springbootlogback/SpringbootLogbackApplicationTests.java
@@ -0,0 +1,12 @@
+package com.johann.springbootlogback;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class SpringbootLogbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
